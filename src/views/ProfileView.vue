fa2b5854e315148ea6733795725a4caa415b6105
Sean <91260510+seanwhitee@users.noreply.github.com>
2024-03-22 17:19:06 +0800

    Update ProfileView and ProjectsView

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -22,6 +22,23 @@ const resourceLink = [
   <Navbar />
   <Sidebar />
   <div class="flex flex-col items-center justify-center m-x-auto mt-40 pb-20">
+    <div
+        aria-hidden="true"
+        class="pointer-events-none absolute top-0 -z-10 transform-gpu overflow-hidden blur-3xl"
+      >
+        <div
+          style="
+            clip-path: polygon(
+              74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 
+              47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%
+            );
+            
+          "
+          class="relative right-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] translate-x-36 -translate-y-40 
+          rotate-[30deg] bg-gradient-to-tr from-[#65c9ff] to-[#4e47b6] opacity-30 sm:right-[calc(50%-30rem)] 
+          sm:w-[72.1875rem]"
+        ></div>
+      </div>
     <!-- profile container div-->
     <div class="flex items-start justify-start text-white mb-8 gap-10 w-3/5">
       <img
@@ -64,13 +81,16 @@ const resourceLink = [
           </a>
         </div>
       </div>
+
+      
     </div>
 
     <div
       class="mt-10 mb-20 text-black w-3/5 text-sm flex justify-start items-center"
     >
       <button
-        class="flex bg-white items-center justify-center hover:bg-white/80 rounded-xl py-2 px-4 transition duration-300 ease-in-out"
+        class="flex bg-white items-center justify-center hover:bg-white/80 rounded-xl py-2 px-4 transition 
+        duration-300 ease-in-out"
         @click="() => router.push('/projects')"
       >
         <span class="me-2">SEE MY PROJECTS HERE</span>
@@ -81,6 +101,7 @@ const resourceLink = [
         />
       </button>
     </div>
+
     <div class="text-white flex items-start w-3/5 m-auto">
       <div class="me-10">About</div>
       <div class="flex flex-col gap-6 w-4/5">
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -24,8 +24,45 @@ fetchProjectsData();
   <Navbar />
   <Sidebar />
   <div
-    class=" pt-24 pb-14 h-full overflow-scroll w-4/6 m-auto flex flex-col items-center justify-start"
+    aria-hidden="true"
+    class="pointer-events-none fixed top-0 -z-10 transform-gpu overflow-hidden blur-3xl"
   >
-    <ProjectCard v-for="repo in repos" :key="repo.id" :programmingLanguage="repo.language" :repoTitle="repo.name" :repoDescription="repo.description" :repoLink="repo.html_url" />
+    <div
+      style="
+        clip-path: polygon(
+          74.1% 44.1%,
+          100% 61.6%,
+          97.5% 26.9%,
+          85.5% 0.1%,
+          80.7% 2%,
+          72.5% 32.5%,
+          60.2% 62.4%,
+          52.4% 68.1%,
+          47.5% 58.3%,
+          45.2% 34.5%,
+          27.5% 76.7%,
+          0.1% 64.9%,
+          17.9% 100%,
+          27.6% 76.8%,
+          76.1% 97.7%,
+          74.1% 44.1%
+        );
+      "
+      class="relative right-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] translate-x-36 -translate-y-40 
+      rotate-[30deg] bg-gradient-to-tr from-[#65c9ff] to-[#4e47b6] opacity-30 sm:right-[calc(50%-30rem)] 
+      sm:w-[72.1875rem]"
+    ></div>
+  </div>
+  <div
+    class="pt-24 pb-14 h-full overflow-scroll w-4/6 m-auto flex flex-col items-center justify-start"
+  >
+    <ProjectCard
+      v-for="repo in repos"
+      :key="repo.id"
+      :programmingLanguage="repo.language"
+      :repoTitle="repo.name"
+      :repoDescription="repo.description"
+      :repoLink="repo.html_url"
+    />
   </div>
 </template>
