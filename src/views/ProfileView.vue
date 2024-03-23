516659487f1bb93122d977f1ac60025fce48ce25
Sean <91260510+seanwhitee@users.noreply.github.com>
2024-03-23 22:50:02 +0800

    Add GradientFog component and refactor ProfileView.vue

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import GradientFog from "@/components/GradientFog.vue";
 import Sidebar from "@/components/navigation/Sidebar.vue";
 import GithubIcon from "@/assets/icons/github.png";
 import LinkedInIcon from "@/assets/icons/linkedin.png";
@@ -22,23 +23,7 @@ const resourceLink = [
   <Navbar />
   <Sidebar />
   <div class="flex flex-col items-center justify-center m-x-auto mt-40 pb-20">
-    <div
-        aria-hidden="true"
-        class="pointer-events-none absolute top-0 -z-10 transform-gpu w-full overflow-hidden blur-3xl"
-      >
-        <div
-          style="
-            clip-path: polygon(
-              74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 
-              47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%
-            );
-            
-          "
-          class="relative right-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] translate-x-36 -translate-y-40 
-          rotate-[30deg] bg-gradient-to-tr from-[#65c9ff] to-[#4e47b6] opacity-30 sm:right-[calc(50%-30rem)] 
-          sm:w-[72.1875rem]"
-        ></div>
-      </div>
+    <GradientFog />
     <!-- profile container div-->
     <div class="flex items-start justify-start text-white mb-8 gap-10 w-3/5">
       <img
